9c0317023bbc7043b7c7a3332e891aa0b80d40b9
RavinderSinghInfostride <ravinder.singh@infostride.com>
2022-09-14 18:33:56 +0530

    Login with Data Driven and Extent Report

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,8 +1,14 @@
 package Pages;
 
 import net.jodah.failsafe.internal.util.Assert;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class LoginPage {
     WebDriver driver;
@@ -12,15 +18,24 @@ public class LoginPage {
     By loginButton = By.xpath("//button[contains(@class,'login-button')]");
     By dashboardPageVerify = By.xpath("//h1[contains(text(),'Dashboard')]");
 
-    String email = "admin@yourstore.com";
-    String password = "admin";
     String dashboardPageText = "Dashboard";
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void login() {
+    public void login() throws IOException {
+        String path = System.getProperty("user.dir")+"//src//test//java//TestData//TestDataFile.xlsx";
+        FileInputStream prop1 = null;
+        try{
+            prop1 = new FileInputStream(path);
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        XSSFWorkbook wb = new XSSFWorkbook(prop1);
+        XSSFSheet sheet = wb.getSheet("Sheet1");
+        String email = sheet.getRow(1).getCell(0).getStringCellValue();
+        String  password = sheet.getRow(1).getCell(1).getStringCellValue();
         driver.findElement(emailInput).clear();
         driver.findElement(passwordInput).clear();
         driver.findElement(emailInput).sendKeys(email);
